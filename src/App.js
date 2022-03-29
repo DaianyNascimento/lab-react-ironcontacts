f130f9a9bd38ae8484bb58f594a8b074e361a512
DaianyNascimento <daiany.cristina@outlook.com>
2022-03-29 18:34:07 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import AllContacts from './contacts.json';
+//import { celebs } from "react";
+const celebs = AllContacts.splice(0, 6);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {celebs.map((elem, index) => {
+            return (
+              <tr>
+                <td>
+                  <img
+                    src={elem.pictureUrl}
+                    alt="celeb pic"
+                    style={{ height: "100px" }}
+                  />
+                </td>
+                <td>
+                  <h3>{elem.name}</h3>
+                </td>
+                <td>
+                  <h3>{(elem.popularity).toFixed(2)}</h3>
+                </td>
+                <td>
+                  <h3>{elem.wonOscar ? "trophy" : "no trophy"}</h3>
+                </td>
+                <td>
+                  <h3>{elem.wonEmmy ? "emmy" : "no emmy"}</h3>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div >
   );
 }
 
